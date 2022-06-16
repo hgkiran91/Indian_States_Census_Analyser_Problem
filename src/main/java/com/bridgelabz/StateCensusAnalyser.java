95a4aa6e3beab8a8692074f0fd05e8a39fadd167
kiran@DESKTOP-P65JLIU <kiranmcehsn@gmail.com>
2022-06-16 14:18:49 +0530

    initial commit

---
+++ src/main/java/com/bridgelabz/StateCensusAnalyser.java
@@ -0,0 +1,7 @@
+package com.bridgelabz;
+
+public class StateCensusAnalyser {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Indian States Census Analyser Problem");
+    }
+}
